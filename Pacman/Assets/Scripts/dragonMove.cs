94a6166643e1f93c7110d48e5ffeb317adb371bd
Tada33 <tadanari33@gmail.com>
2021-09-27 09:57:26 +1000

    Character movement updated

--- Pacman/Assets/Scripts/dragonMove.cs
+++ Pacman/Assets/Scripts/dragonMove.cs
@@ -2,24 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class dragonMove : MonoBehaviour
 {
-    float speed = 1;
+  
+    [SerializeField] Transform[] Positions;
+    [SerializeField] float speed;
+    Transform NextPos;
+
+    int NextPosIndex;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.Translate(0, 0, 0);
+        NextPos = Positions[0];
+       
     }
    
    
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
+       
     {
-        this.transform.Translate(5, 0, 0/ 50);
-        this.transform.Translate(5, -4, 0);
-        this.transform.Translate(0, -4, 0);
-        this.transform.Translate(0, 0, 0);
+        MoveDragon();
+
+       
     }
-    
+    void MoveDragon()
+    {
+        if (transform.position == NextPos.position)
+        {
+            NextPosIndex++;
+            if (NextPosIndex >= Positions.Length)
+            {
+                NextPosIndex = 0;
+            }
+            NextPos = Positions[NextPosIndex];
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, NextPos.position, speed * Time.deltaTime);
+        }
+    }
 }
